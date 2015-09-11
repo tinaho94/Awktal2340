96a529e6ceecdabcefcb0ef17d8db75a0e8572b6
Henry Peteet <hvpeteet@Henrys-MacBook-Pro.local>
2015-09-11 10:53:08 -0400

    Added a gradle file and appropriate directories to be able to build and run with gradle commands. Also added a .gitignore so that you don't have to worry about accidentaly uploading the autogenerated files from gradle. Also added a basic javafx hello world app where our MainApp.java will live.

---
+++ src/main/java/awkMULE/MainApp.java
@@ -0,0 +1,39 @@
+package awkMULE;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class MainApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("MULE");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
+// public class MainApp {
+//     public static void main(String[] args) {
+//         System.out.println("Hey there");
+//     }
+// }
