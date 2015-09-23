92c638128d4d096e441496b699438698e75c7e49
Alex Loomis <alex.v.loomis@gmail.com>
2015-09-23 16:31:59 -0400

    Town map made prettier and not sucky

--- src/main/java/awktal/mule/TownController.java
+++ src/main/java/awktal/mule/TownController.java
@@ -15,12 +15,16 @@ public class TownController extends SceneController {
     @FXML
     Button return_button;
     @FXML
-    AnchorPane dat_pain;
+    AnchorPane main_pane;
+    @FXML
+    AnchorPane town_pane;
 
     @FXML
     private void initialize() {
-        Image img = new Image(TownController.class.getResourceAsStream("map_pictures/pallet_town.jpg"));
-        dat_pain.setBackground(new Background(new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
+        Image img = new Image(TownController.class.getResourceAsStream("town_pictures/town.png"));
+        BackgroundSize size = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
+        // Image img = new Image(TownController.class.getResourceAsStream("town_pictures/pallet_town.jpg"));
+        town_pane.setBackground(new Background(new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, size)));
     }
 
 
