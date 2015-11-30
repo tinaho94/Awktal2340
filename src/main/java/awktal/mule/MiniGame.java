01a556aade91df90179e34793fad72d8fc833ca8
Stephen Zolnik <sjzolnik@hotmail.com>
2015-11-30 13:55:09 -0500

    used Assay building for minigame

--- src/main/java/awktal/mule/MiniGame.java
+++ src/main/java/awktal/mule/MiniGame.java
@@ -1,12 +1,25 @@
 package awktal.mule;
+import javafx.fxml.FXML;
 
 
-public class MiniGame {
+public class MiniGame extends SceneController{
 	
+
+
+
 
 	public MiniGame() {
 
 	}
 
 
+
+
+    /**
+     * Will be called after the constructor when the fxml is loaded.
+    */
+    @FXML
+    private void initialize() {
+        System.out.println("hi");//play.musicMp3(openRange);
+    }
 }
